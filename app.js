ff5ad18171dfb6358e7ddcb998d97d098f7bbea8
Harshith Lanka <98058276+harshithlanka3@users.noreply.github.com>
2022-12-25 17:24:46 +0530

    Added all operation functions and round function

---
+++ app.js
@@ -0,0 +1,21 @@
+// Operation functions
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+// Taken from: https://www.jacklmoore.com/notes/rounding-in-javascript/
+function round(value, decimals) {
+    return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
