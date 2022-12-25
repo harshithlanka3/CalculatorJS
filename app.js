70f4c5a935e884509ddb191705a6610d3704520e
Harshith Lanka <98058276+harshithlanka3@users.noreply.github.com>
2022-12-25 17:27:53 +0530

    Added operate function that isolates boolean logic

--- app.js
+++ app.js
@@ -15,6 +15,21 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+function operate(operator, num1, num2) {
+    if (operator === '+') {
+        return add(num1, num2);
+    }
+    if (operator === '-') {
+        return subtract(num1, num2);
+    }
+    if (operator === '*') {
+        return multiply(num1, num2);
+    }
+    if (operator === '/') {
+        return divide(num1, num2);
+    }
+}
+
 // Taken from: https://www.jacklmoore.com/notes/rounding-in-javascript/
 function round(value, decimals) {
     return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
